1940a9692be302041f16c2a9a6f43ba4bb963766
João Pedro Gomes Pinheiro <jppinheiro171@gmail.com>
2018-10-17 16:34:42 -0300

    Update aula4_Joao_Pedro.py

--- aula4_Joao_Pedro.py
+++ aula4_Joao_Pedro.py
@@ -67,3 +67,50 @@ def grid():
     line()
 
 grid()
+
+#exercício 4
+print('exercício 4: ')
+print(time.localtime())
+
+print('-------------')
+
+#exercício 5
+print('exercício 5: ')
+def fatorial(n):
+	fatorial=1
+
+	for i in range(n):
+		fatorial=fatorial*(i+1)
+	return fatorial
+
+print ('o fatorial do número 6 é =',fatorial(6))
+
+print('-------------')
+
+#exercício 6
+print('exercício 6: ')
+def comparar(a,b):
+	if (a>b):
+		print ('o número ',a,'é maior que o número',b)	
+		print ('o número maior é = ',a)	
+	if (a<b):
+		print ('o número ',a,'é menor que o número',b)
+		print ('o número maior é = ',b)	
+	if (a==b):
+		print ('o número',a,'é igual ao número',b)
+#Escrevendo um exemplo
+comparar(10,11)
+
+print('-------------')
+
+#exercício 7
+print('exercício 7: ')
+def comparar(a,b):
+	if (a>b):
+		print ('(',b,', ',a,')')	
+	if (a<b):
+		print ('(',a,', ',b,')')	
+	if (a==b):
+		print ('(',a,', ',b,')')	
+#Escrevendo um exemplo
+comparar(17,12)
